978701296b3f1200e5026c1c1df109390b1c0275
alumno <alumno@Instituto.local>
2025-05-21 08:24:56 -0300

    ArreglosyRepeticiones

---
+++ script.js
@@ -0,0 +1,50 @@
+//Ejercicio 1
+
+//1
+
+let frutas = ['Manzana', 'Naranja', 'Mandarina']
+
+
+//2
+frutas.push('Durazno')
+
+//3
+
+frutas.unshift('Ciruela')
+
+//4
+
+frutas.pop()
+
+//5
+
+frutas.shift()
+
+//6
+
+console.log(frutas)
+
+//Ejercicio 2
+
+//1
+
+let nombres = ['Messi', 'Julian', 'Di Maria', 'De Paul', 'Dibu']
+
+//2
+
+console.log(nombres.length)
+
+//3
+
+nombres.push('Cuti', 'Alexis')
+
+
+//4
+
+console.log(nombres.length)
+
+//Ejercicio 3
+
+//1
+
+let numeros = [0, 1, 2, 3, 4]
